cb0c9a9f76f0328dee3bf64d60e3bd19287ccbaf
Troels Schwarz-Linnet <tlinnet@gmail.com>
2017-11-28 22:53:04 +0100

    Auto running notebooks

--- examples/make_notebooks_from_py.py
+++ examples/make_notebooks_from_py.py
@@ -2,6 +2,7 @@ from py2nb.tools import python_to_notebook
 import os.path
 import os
 import glob
+import subprocess
 
 for filepath in glob.iglob('**/*.py', recursive=True):
 #for filepath in glob.iglob('*.py'):
@@ -14,5 +15,10 @@ for filepath in glob.iglob('**/*.py', recursive=True):
     if os.path.isfile(f+'.ipynb'):
         print(".ipynb notebook already exists:", filepath)
     else:
+        # Convert
         python_to_notebook(f+'.py', f+'.ipynb')
+        # Execute it
+        subprocess.call(["jupyter", "nbconvert", "--allow-errors", "--ExecutePreprocessor.timeout=30", "--to", "notebook", "--execute", f+'.ipynb', "--output", f+'.ipynb'])
+        # Trust notebook
+        subprocess.call(["jupyter", "trust", f+'.ipynb'])
         print("Converting to .ipynb notebook:", filepath)
