6d69ec47de9a3f2b32a6da113fb3ac21f924df6d
Troels Schwarz-Linnet <tlinnet@gmail.com>
2017-11-30 11:05:38 +0100

    Updated setup file, to fix README

--- setup.py
+++ setup.py
@@ -7,14 +7,22 @@ from codecs import open
 from os import path
 from drawnmr import __version__
 
-here = path.abspath(path.dirname(__file__))
+# Determine install position
+package_name = 'drawnmr'
+import sys
+from site import USER_SITE, USER_BASE
+rela_path = USER_SITE.split(USER_BASE)[-1][1:]
+install_to = path.join(sys.prefix, rela_path, package_name)
+print(rela_path)
+print(install_to)
 
 # get long description from README
+here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='drawnmr',
+    name=package_name,
     version=__version__,
     description='A module for displaying NMR data with bokeh in Python or Jupyter Notebooks.',
     long_description=long_description,
@@ -34,7 +42,6 @@ setup(
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX :: Linux'],
     requires=['bokeh', 'numpy', 'matplotlib', 'nmrglue', 'scipy'],
-    packages=[
-        'drawnmr'],
-    data_files=[('', ['README.rst', 'LICENSE'])],
+    packages=['drawnmr'],
+    data_files=[(rela_path, ['README.rst', 'LICENSE'])],
 )
